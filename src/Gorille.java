3b31b319072c443cb257e4409cb63de1402d80b7
Mathis934 <m.semery@lprs.fr>
2025-02-11 09:33:06 +0100

    initialisation

---
+++ src/Gorille.java
@@ -0,0 +1,25 @@
+public class Gorille {
+    public void afficherGorille(){
+        System.out.println("▒▒▒▒▒▄██████████▄▒▒▒▒▒\n" +
+                "▒▒▒▄██████████████▄▒▒▒\n" +
+                "▒▒██████████████████▒▒\n" +
+                "▒▐███▀▀▀▀▀██▀▀▀▀▀███▌▒\n" +
+                "▒███▒▒▌■▐▒▒▒▒▌■▐▒▒███▒\n" +
+                "▒▐██▄▒▀▀▀▒▒▒▒▀▀▀▒▄██▌▒\n" +
+                "▒▒▀████▒▄▄▒▒▄▄▒████▀▒▒\n" +
+                "▒▒▐███▒▒▒▀▒▒▀▒▒▒███▌▒▒\n" +
+                "▒▒███▒▒▒▒▒▒▒▒▒▒▒▒███▒▒\n" +
+                "▒▒▒██▒▒▀▀▀▀▀▀▀▀▒▒██▒▒▒\n" +
+                "▒▒▒▐██▄▒▒▒▒▒▒▒▒▄██▌▒▒▒\n" +
+                "▒▒▒▒▀████████████▀▒▒▒▒");
+    }
+    public void manger(){
+        System.out.println("le gorille mange");
+    }
+    public void dormir(){
+        System.out.println("le gorille dort");
+    }
+    public void crier(){
+        System.out.println("le gorille crie");
+    }
+}
